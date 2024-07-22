afa6acbc8cfa9c36959da215c4af976666c337d9
Harika <harikamullagura11@gmail.com>
2024-07-22 15:18:18 +0530

    commit changes 22 Jul 2024

---
+++ src/ex_20072024/Lab086.java
@@ -0,0 +1,14 @@
+package ex_20072024;
+
+public class Lab086 {
+    public static void main(String[] args) {
+        //Modulus - %
+        int num = 22;
+        if(num % 2 == 0){
+            System.out.println("The number is even " +num);
+        }else{
+            System.out.println("The number is odd" + num);
+        }
+        }
+    }
+
---
+++ src/ex_20072024/Lab087.java
@@ -0,0 +1,21 @@
+package ex_20072024;
+
+import java.util.Scanner;
+
+public class Lab087 {
+    public static void main(String[] args) {
+        // Take user input and check if number is even or odd
+        Scanner sc = new Scanner(System.in); //creation of object for the scanner class
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        System.out.println(num);
+
+        if(num%2==0){
+            System.out.println("Num is Even");
+        }else {
+            System.out.println("Num is Odd");
+        }
+
+
+    }
+}
